8c44b3b8c4c696de19baf8db20c78e2bb9aff9f6
Theo Cincotta <theo@insight360.io>
2018-04-18 09:05:46 -0700

    added a command line argument for seconds between runs. bash slow_data.sh 1 (runs every second, default is 5 seconds)

--- hf-api/slow_data.sh
+++ hf-api/slow_data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sleeptime=$1
+
+if [ "$1" == "" ]; then
+echo "sleep 5; echo will be set"
+sleeptime=5
+fi
 
 while true
  do
@@ -6,5 +12,5 @@ while true
   randomp2=`echo $((10 + RANDOM % 100))`
   curl -X POST http://localhost:3000/?p1=${randomp1}
   curl -X POST http://localhost:3000/?p2=${randomp2}
- sleep 5
+ sleep $sleeptime
 done
