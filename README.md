8b5fa4eb55193ebc8b1eb37371d0404869f09681
Theo Cincotta <theo@insight360.io>
2018-04-18 10:46:57 -0700

    dded README.md and removed virtualenv.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # hf-api
+
+hf-api is a request aggregator that saves the count, average, maximum, minimum for each key sent.
+
+## Quick Start
+```
+mkdir ~/src
+cd ~/src
+git clone https://github.com/theologian/hf-api.git
+cd hf-api
+vagrant up
+vagrant ssh 
+bash /vagrant/setup.sh
+nohup python /vagrant/hf-api/app.py &
+bash /vagrant/hf-api/request_generator.sh 2
+```
--- setup.sh
+++ setup.sh
@@ -3,11 +3,9 @@
 sudo apt update
 sudo apt-get install -y python-setuptools python-dev \
 build-essential python-virtualenv python-pip redis-server
-
-mkdir  ~/virtualenv/hf-api/
-sudo -H -u vagrant virtualenv ~/virtualenv/hf-api
 pip install -r /vagrant/requirements.txt
 
-source ~/virtualenv/hf-api/bin/activate
-
+#mkdir  ~/virtualenv/hf-api/
+#virtualenv ~/virtualenv/hf-api
+#. ~/virtualenv/hf-api/bin/activate
 #while true; do curl -X POST "http://localhost:3000/?p1=2&p2=99"; sleep 5; done
